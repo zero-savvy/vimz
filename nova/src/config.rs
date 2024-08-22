9443de16e2143c737ece0b2129af25295469b526
Piotr Mikołajczyk <piomiko41@gmail.com>
2024-08-22 17:03:23 +0200

    Rework VimZ configuration

---
+++ nova/src/config.rs
@@ -0,0 +1,56 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(
+    version,
+    author,
+    about = "Prove the truthfulness of your media! \nThe naming rationale: Verifiable Image Manipulation based on ZKP. \nPronunciation: /ˈwɪmzi/, just like whimsy :D",
+    long_about = None,
+)]
+pub struct Config {
+    /// The JSON file containing the original and the transformed image data to verify.
+    #[clap(short, long)]
+    pub input: PathBuf,
+
+    ///This file will contain the final Proof to be verified by others.
+    #[clap(short, long)]
+    pub output: PathBuf,
+
+    /// The R1CS file of the compiled Circom circuit.
+    #[clap(short, long)]
+    pub circuit: PathBuf,
+
+    /// Witness generator file of the circuit.
+    #[clap(short, long)]
+    pub witness_generator: PathBuf,
+
+    /// The transformation function.
+    #[clap(short, long)]
+    pub function: String,
+
+    /// The resolution of the image.
+    #[clap(short, long)]
+    pub resolution: String,
+}
+
+impl Config {
+    pub fn display(&self) {
+        println!(" ________________________________________________________");
+        println!("                                                         ");
+        println!(" ██     ██  ██  ███    ███  ████████   Verifiable  Image");
+        println!(" ██     ██  ██  ████  ████      ███    Manipulation from");
+        println!("  ██   ██   ██  ██ ████ ██     ██      Folded   zkSNARKs");
+        println!("   ██ ██    ██  ██  ██  ██   ███                         ");
+        println!("    ███     ██  ██      ██  ████████████ v1.3.0 ████████");
+        println!(" ________________________________________________________");
+        println!("| Input file: {:?}", self.input);
+        println!("| Output file: {:?}", self.output);
+        println!("| Selected function: {}", self.function);
+        println!("| Circuit file: {:?}", self.circuit);
+        println!("| Witness generator: {:?}", self.witness_generator);
+        println!("| Image resolution: {}", self.resolution);
+        println!(" ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾");
+    }
+}
--- nova/src/main.rs
+++ nova/src/main.rs
@@ -1,9 +1,11 @@
+mod config;
+
 use std::{env::current_dir, fs::File, io::Read, str::FromStr, time::Instant};
 
 use ark_bn254::{constraints::GVar, Bn254, Fr, G1Projective as G1};
 use ark_groth16::Groth16;
 use ark_grumpkin::{constraints::GVar as GVar2, Projective as G2};
-use clap::{App, Arg};
+use clap::Parser;
 use serde::Deserialize;
 use sonobe::{
     commitment::{kzg::KZG, pedersen::Pedersen},
@@ -13,38 +15,33 @@ use sonobe::{
     Decider as _, FoldingScheme,
 };
 
+use crate::config::Config;
+
 #[derive(Deserialize)]
 struct ZKronoInput {
     original: Vec<Vec<String>>,
     transformed: Vec<Vec<String>>,
 }
 
-fn fold_fold_fold(
-    selected_function: String,
-    circuit_filepath: String,
-    witness_gen_filepath: String,
-    output_file_path: String,
-    input_file_path: String,
-    resolution: String,
-) {
+fn fold_fold_fold(config: &Config) {
     println!(
-        "Running NOVA with witness generator: {} and group: {}",
-        witness_gen_filepath,
+        "Running NOVA with witness generator: {:?} and group: {}",
+        config.witness_generator,
         std::any::type_name::<G1>()
     );
     let mut iteration_count = 720; // HD
-    if resolution == "4K" {
+    if config.resolution == "4K" {
         iteration_count = 2160;
     }
-    if resolution == "8K" {
+    if config.resolution == "8K" {
         iteration_count = 4320;
     }
     let root = current_dir().unwrap();
 
-    let circuit_file = root.join(circuit_filepath);
-    let witness_generator_file = root.join(witness_gen_filepath);
+    let circuit_file = root.join(&config.circuit);
+    let witness_generator_file = root.join(&config.witness_generator);
 
-    let mut input_file = File::open(input_file_path.clone()).expect("Failed to open the file");
+    let mut input_file = File::open(&config.input).expect("Failed to open the file");
     let mut input_file_json_string = String::new();
     input_file
         .read_to_string(&mut input_file_json_string)
@@ -136,97 +133,7 @@ fn fold_fold_fold(
 }
 
 fn main() {
-    let matches = App::new("VIMz")
-        .version("v1.3.0")
-        .author("Zero-Savvy")
-        .about("Prove the truthfulness of your media! \n The naming rationale: Verifiable Image Manipulation based on ZKP. \n Pronunciation: /ˈwɪmzi/, just like whimsy :D")
-        .arg(
-            Arg::with_name("input")
-            .required(true)
-            .short("i")
-            .long("input")
-            .value_name("FILE")
-            .help("The JSON file containing the original and the transformed image data to verify.")
-            .takes_value(true)
-        )
-        .arg(
-            Arg::with_name("output")
-            .required(true)
-            .short("o")
-            .long("output")
-            .value_name("FILE")
-            .help("This file will contain the final Proof to be verified by others.")
-            .takes_value(true)
-        )
-        .arg(
-            Arg::with_name("circuit")
-            .required(true)
-            .short("c")
-            .long("circuit")
-            .value_name("R1CS FILE")
-            .help("The R1CS file of the compiled Circom circuit.")
-            .takes_value(true)
-        )
-        .arg(
-            Arg::with_name("witnessgenerator")
-            .required(true)
-            .short("w")
-            .long("witnessgenerator")
-            .value_name("BINARY/WASM FILE")
-            .help("Witness generator file of the circuit.")
-            .takes_value(true)
-        )
-        .arg(
-            Arg::with_name("function")
-            .required(true)
-            .short("f")
-            .long("function")
-            .value_name("FUNCTION")
-            .help("The transformation function.")
-            .takes_value(true)
-            .possible_values(&["crop", "fixedcrop", "grayscale", "resize", "color_transform", "sharpness", "contrast", "blur", "brightness", "hash"])
-        )
-        .arg(
-            Arg::with_name("resolution")
-            .required(true)
-            .short("r")
-            .long("resolution")
-            .value_name("RESOLUTION")
-            .help("The resolution of the image.")
-            .takes_value(true)
-            .possible_values(&["SD", "HD", "FHD", "4K", "8K"])
-        )
-        .get_matches();
-
-    let witness_gen_filepath = matches.value_of("witnessgenerator").unwrap();
-    let circuit_filepath = matches.value_of("circuit").unwrap();
-    let output_filepath = matches.value_of("output").unwrap();
-    let input_filepath = matches.value_of("input").unwrap();
-    let selected_function = matches.value_of("function").unwrap();
-    let resolution = matches.value_of("resolution").unwrap();
-
-    println!(" ________________________________________________________");
-    println!("                                                         ");
-    println!(" ██     ██  ██  ███    ███  ████████   Verifiable  Image");
-    println!(" ██     ██  ██  ████  ████      ███    Manipulation from");
-    println!("  ██   ██   ██  ██ ████ ██     ██      Folded   zkSNARKs");
-    println!("   ██ ██    ██  ██  ██  ██   ███                         ");
-    println!("    ███     ██  ██      ██  ████████████ v1.3.0 ████████");
-    println!(" ________________________________________________________");
-    println!("| Input file: {}", input_filepath);
-    println!("| Output file: {}", output_filepath);
-    println!("| Selected function: {}", selected_function);
-    println!("| Circuit file: {}", circuit_filepath);
-    println!("| Witness generator: {}", witness_gen_filepath);
-    println!("| Image resolution: {}", resolution);
-    println!(" ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾");
-
-    fold_fold_fold(
-        selected_function.to_string().clone(),
-        circuit_filepath.to_string().clone(),
-        witness_gen_filepath.to_string(),
-        output_filepath.to_string(),
-        input_filepath.to_string(),
-        resolution.to_string(),
-    );
+    let config = Config::parse();
+    config.display();
+    fold_fold_fold(&config);
 }

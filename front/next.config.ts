6131047812787d49f9f2321d6023bf910571dc5a
lovely-necromancer <lovely_necromancer@proton.me>
2024-11-19 15:08:11 +0100

    add web-based ui with support of some functions

---
+++ front/next.config.ts
@@ -0,0 +1,22 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      assert: require.resolve('assert'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      os: require.resolve('os-browserify'),
+    }
+    return config;
+  },
+};
+
+export default nextConfig;

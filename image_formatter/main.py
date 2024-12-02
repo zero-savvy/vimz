2c1d35e6d8a7395fa8411c76b4a1657703c1b356
Piotr Mikołajczyk <piomiko41@gmail.com>
2024-12-02 12:03:37 +0100

    Refine compressing and convolution

--- image_formatter/main.py
+++ image_formatter/main.py
@@ -4,6 +4,7 @@ import json
 from PIL import Image
 import numpy as np
 
+from img_utils import compress, conv2d
 from plotting import plot_images_side_by_side
 
 
@@ -12,59 +13,6 @@ def get_image_path():
     root.withdraw()
     file_path = filedialog.askopenfilename()
     return file_path
-
-
-def compress(image_in):
-    array_in = np.array(image_in).tolist()
-    output_array = []
-    # print(len(array_in), len(array_in[0]), len(array_in[0][0]))
-
-    for i in range(0, len(array_in)):
-        row = []
-        hexValue = ''
-        for j in range(0, len(array_in[i])):
-            if np.isscalar(array_in[i][j]):
-                hexValue = hex(int(array_in[i][j]))[2:].zfill(6) + hexValue
-            else:
-                for k in range(0, 3):
-                    hexValue = hex(int(array_in[i][j][k]))[2:].zfill(2) + hexValue
-            if j % 10 == 9:
-                row.append("0x" + hexValue)
-                hexValue = ''
-        output_array.append(row)
-    return output_array
-
-
-def conv2d(array, kernel, weight=1):
-    # Get the dimensions of the input array and kernel
-    array_height, array_width = len(array), len(array[0])
-    kernel_height, kernel_width = len(kernel), len(kernel[0])
-
-    # Extend the array with zeros
-    border_size = kernel_height // 2
-    extended = [[0 for _ in range(array_width + border_size * 2)] for _ in range(array_height + border_size * 2)]
-    for i in range(array_height):
-        for j in range(array_width):
-            extended[i+border_size][j+border_size] = array[i][j]
-    
-    # Initialize the output (convolved) array
-    convolved_array = [[0 for _ in range(array_width)] for _ in range(array_height)]
-
-    # Perform the convolution using nested loops
-    for i in range(array_height):
-        for j in range(array_width):
-            conv_value = 0
-            for m in range(kernel_height):
-                for n in range(kernel_width):
-                    conv_value += extended[i + m][j + n] * kernel[m][n]
-            convolved_array[i][j] = conv_value // weight
-            if convolved_array[i][j] > 255:
-                convolved_array[i][j] = 255
-            elif convolved_array[i][j] < 0:
-                convolved_array[i][j] = 0
-            # convolved_array[i][j] = extended[i][j]
-
-    return convolved_array
 
 
 def sharppen_image(image_path):
@@ -118,14 +66,17 @@ def adjust_contrast(image_path, desired_contrast):
         # r_mean = int(np.mean(r_channel) *1000)
         # b_mean = int(np.mean(b_channel) *1000)
         # g_mean = int(np.mean(b_channel) *1000)
-        r_mean = int(128 *1000)
-        b_mean = int(128 *1000)
-        g_mean = int(128 *1000)
-        r_adjusted = ((r_channel - float(r_mean) / 1000) * desired_contrast + float(r_mean) / 1000).clip(0, 255).astype(np.uint8)
-        g_adjusted = ((g_channel - float(g_mean) / 1000) * desired_contrast + float(g_mean) / 1000).clip(0, 255).astype(np.uint8)
-        b_adjusted = ((b_channel - float(b_mean) / 1000) * desired_contrast + float(b_mean) / 1000).clip(0, 255).astype(np.uint8)
+        r_mean = int(128 * 1000)
+        b_mean = int(128 * 1000)
+        g_mean = int(128 * 1000)
+        r_adjusted = ((r_channel - float(r_mean) / 1000) * desired_contrast + float(r_mean) / 1000).clip(0, 255).astype(
+            np.uint8)
+        g_adjusted = ((g_channel - float(g_mean) / 1000) * desired_contrast + float(g_mean) / 1000).clip(0, 255).astype(
+            np.uint8)
+        b_adjusted = ((b_channel - float(b_mean) / 1000) * desired_contrast + float(b_mean) / 1000).clip(0, 255).astype(
+            np.uint8)
         adjusted_image = np.dstack((r_adjusted, g_adjusted, b_adjusted))
-        
+
         plot_images_side_by_side(image_np, adjusted_image)
         return compress(adjusted_image)
 
@@ -133,7 +84,7 @@ def adjust_contrast(image_path, desired_contrast):
 def compress_image(image_path):
     with Image.open(image_path) as image:
         return compress(image)
-    
+
 
 def adjust_brightness(np_image, brightness_factor):
     with Image.open(image_path) as image:
@@ -143,33 +94,33 @@ def adjust_brightness(np_image, brightness_factor):
         adjusted_image = np.clip(adjusted_image_float, 0, 255).astype(np.uint8)
 
         plot_images_side_by_side(image_np, adjusted_image)
-    
-        return compress(adjusted_image)
-    
 
-def crop_image(image_path, x: int, y:int, new_width: int, new_height:int):
+        return compress(adjusted_image)
+
+
+def crop_image(image_path, x: int, y: int, new_width: int, new_height: int):
     with Image.open(image_path) as image:
         image_np = np.array(image)
-        adjusted_image = image_np[y:y+new_height, x:x+new_width]
+        adjusted_image = image_np[y:y + new_height, x:x + new_width]
         # adjusted_image = [[image_np[i][j] for j in range(x, x+new_width)] for i in range(y, y+new_height)]
         plot_images_side_by_side(image_np, adjusted_image)
-    
-        return compress(adjusted_image)
-    
 
-def resize_image(image_path, new_height:int, new_width: int):
+        return compress(adjusted_image)
+
+
+def resize_image(image_path, new_height: int, new_width: int):
     with Image.open(image_path) as image:
         # image_np = np.array(image)
         # adjusted_image = image.resize((new_width, new_height), resample=Image.Resampling.BILINEAR)
         # adjusted_image_np = np.array(adjusted_image)
 
         # adjusted_image = [[image_np[i][j] for j in range(x, x+new_width)] for i in range(y, y+new_height)]
-        
+
         img_array = np.array(image)
 
         # Get the dimensions of the original image
         height, width, channels = img_array.shape
-        
+
         # Calculate the scaling factors for each dimension
         # scale_y = (new_height - 1) / (height - 1)
         # scale_x = (new_width - 1) / (width - 1)
@@ -188,7 +139,7 @@ def resize_image(image_path, new_height:int, new_width: int):
                     x_h = int(j * x_ratio) + 1
                     y_l = int(i * y_ratio)
                     y_h = int(i * y_ratio) + 1
-                    
+
                     a = img_array[y_l, x_l]
                     b = img_array[y_l, x_h]
                     c = img_array[y_h, x_l]
@@ -197,7 +148,7 @@ def resize_image(image_path, new_height:int, new_width: int):
                     weight = 2 if i % 2 == 0 else 1
                     weight = float(weight) / 3
                     summ = a * weight + b * weight \
-                        + c * (1 - weight) + d * (1 - weight)
+                           + c * (1 - weight) + d * (1 - weight)
                     new_img_array[i, j] = summ / 2
         else:
             # Perform bilinear interpolation
@@ -207,7 +158,7 @@ def resize_image(image_path, new_height:int, new_width: int):
                     x_h = int(j * x_ratio) + 1
                     y_l = int(i * y_ratio)
                     y_h = int(i * y_ratio) + 1
-                    
+
                     a = img_array[y_l, x_l]
                     b = img_array[y_l, x_h]
                     c = img_array[y_h, x_l]
@@ -227,7 +178,7 @@ image_path = get_image_path()
 
 if image_path:
     # Example usage
-    
+
     # Crop the image and save it
     compressed_original_image = compress_image(image_path)
     out = {
@@ -236,7 +187,7 @@ if image_path:
     print("Image compressed successfully.")
     cmd = int(input("Enter your command (default[1]): 1) crop, 2) resize, 3) greyscale, 4) brightness,"
                     " 5) contrast, 6) sharpen, 7) blur: ") or "1")
-    
+
     if cmd == 1:
         output_path = 'transformation_crop.json'  # Path to save the cropped image
         x = int(input("Enter x coordination:"))
@@ -257,7 +208,7 @@ if image_path:
         compressed_transformed_image = crop_image(image_path, x, y, w, h)
         print("Applied CROP filter successfully.")
 
-        out["info"] = x * 2**24 + y * 2**12
+        out["info"] = x * 2 ** 24 + y * 2 ** 12
 
     elif cmd == 2:
         output_path = 'transformation_resize.json'  # Path to save the resized image
@@ -288,7 +239,7 @@ if image_path:
         desired_brightness = float(input("Enter desired brightness factor (1.00 = no effect):"))
         compressed_transformed_image = adjust_brightness(image_path, desired_brightness)
         print("Applied BRIGHTNESS filter successfully.")
-        
+
         out["transformed"] = compressed_transformed_image
         out["factor"] = int(desired_brightness * 10)
 
@@ -298,15 +249,15 @@ if image_path:
         desired_contrast = float(input("Enter desired contrast (1.00 = no effect):"))
         compressed_transformed_image = adjust_contrast(image_path, desired_contrast)
         print("Applied CONTRAST filter successfully.")
-        
+
         out["transformed"] = compressed_transformed_image
-        out["factor"] = int(desired_contrast*10)
+        out["factor"] = int(desired_contrast * 10)
 
     elif cmd == 6:
         output_path = 'transformation_sharpen.json'  # Path to save the cropped image
         compressed_transformed_image, compressed_zeros = sharppen_image(image_path)
         print("Applied SHARPNESS filter successfully.")
-        
+
         out["transformed"] = compressed_transformed_image
         out["original"] = compressed_zeros + out["original"] + compressed_zeros
 
@@ -314,14 +265,14 @@ if image_path:
         output_path = 'transformation_blur.json'  # Path to save the cropped image
         compressed_transformed_image, compressed_zeros = blur_image(image_path)
         print("Applied BLUR filter successfully.")
-        
+
         out["transformed"] = compressed_transformed_image
         out["original"] = compressed_zeros + out["original"] + compressed_zeros
 
     else:
         print("The entered command was wrong. It should an Integer from 1 to 7.")
         exit()
-    
+
     with open(output_path, 'w') as fp:
         json.dump(out, fp, indent=4)
     print("Image data dumped successfully.")

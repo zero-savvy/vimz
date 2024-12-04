ad8cac08f21540a92ae6c502e83a494c3d9043aa
Piotr Mikołajczyk <piomiko41@gmail.com>
2024-12-04 14:39:06 +0100

    Circuit compilation made better

--- Makefile
+++ Makefile
@@ -2,6 +2,8 @@
 help: # Show help for each of the Makefile recipes.
 	@grep -E '^[a-zA-Z0-9 -]+:.*#'  Makefile | sort | while read -r l; do printf "\033[1;32m$$(echo $$l | cut -f 1 -d':')\033[00m:$$(echo $$l | cut -f 2- -d'#')\n"; done
 
+TRANSFORMATIONS = blur brightness contrast crop grayscale hash resize sharpness
+
 ########################################################################################################################
 ########################### INPUT DATA GENERATION ######################################################################
 ########################################################################################################################
@@ -9,8 +11,6 @@ help: # Show help for each of the Makefile recipes.
 INPUT_SOURCE = --image-path ./source_image/HD.png
 INPUT_TARGET_DIR = ./input_data/
 INPUT_TARGET = --output-dir $(INPUT_TARGET_DIR)
-
-TRANSFORMATIONS = blur brightness contrast crop grayscale hash resize sharpness
 
 EXTRA_ARGS_brightness = --factor 1.4
 EXTRA_ARGS_contrast = --factor 1.4
@@ -24,3 +24,26 @@ generate-input-data: $(patsubst %, $(INPUT_TARGET_DIR)%.json, $(TRANSFORMATIONS)
 $(INPUT_TARGET_DIR)%.json:
 	@mkdir -p $(INPUT_TARGET_DIR)
 	@python3 image_editor/main.py $* $(INPUT_SOURCE) $(INPUT_TARGET) $(EXTRA_ARGS_$*)
+
+########################################################################################################################
+########################### CIRCUIT COMPILATION ########################################################################
+########################################################################################################################
+
+BACKEND = sonobe nova_snark
+
+ARTIFACTS := $(foreach backend,$(BACKEND),$(foreach trans,$(TRANSFORMATIONS), \
+    circuits/$(backend)/$(trans)_step.r1cs \
+    circuits/$(backend)/$(trans)_step_js/$(trans)_step.wasm))
+
+.PHONY: build-circuits
+build-circuits: $(ARTIFACTS)
+
+circuits/%_step.r1cs circuits/%_step_js/%_step.wasm: circuits/%_step.circom
+	@backend=$(word 1,$(subst /, ,$*)); \
+	transformation=$(word 2,$(subst /, ,$*)); \
+	transformation_file=$$transformation\_step.circom; \
+	./circuits/build_circuits.sh circuits/$$backend $$transformation_file
+
+.PHONY: clean-circuits
+clean-circuits:
+	@cd circuits && ./clear_builds.sh sonobe && ./clear_builds.sh nova_snark
--- circuits/build_circuits.sh
+++ circuits/build_circuits.sh
@@ -4,25 +4,19 @@ set -eou pipefail
 
 # Display usage instructions
 usage() {
-    echo "Usage: $0 <subfolder> [specific_circuit_file]"
-    echo " - <subfolder>: Required. The subfolder containing the circuit files."
-    echo " - [specific_circuit_file]: Optional. A specific .circom file to rebuild."
-    exit 1
+  echo "Usage: $0 <subfolder> [specific_circuit_file]"
+  echo " - <subfolder>: Required. The subfolder containing the circuit files."
+  echo " - [specific_circuit_file]: Optional. A specific .circom file to rebuild."
+  exit 1
 }
 
 # Validate arguments
 if [ "$#" -lt 1 ]; then
-    usage
+  usage
 fi
 
 # Assign the first argument as the subfolder name
 subfolder="$1"
-
-# Validate the provided subfolder
-if [ ! -d "$subfolder" ]; then
-    echo "Error: Subfolder '$subfolder' not found."
-    exit 1
-fi
 
 # Navigate to the subfolder
 cd "$subfolder" || exit
@@ -35,52 +29,19 @@ shopt -s nullglob
 
 # Determine files to process
 if [ "$#" -eq 2 ]; then
-    specific_file="$2"
-    if [ -f "$specific_file" ]; then
-        file_list=("$specific_file")
-    else
-        echo "Error: File '$specific_file' not found."
-        exit 1
-    fi
+  specific_file="$2"
+  if [ -f "$specific_file" ]; then
+    file_list=("$specific_file")
+  else
+    echo "Error: File '$specific_file' not found."
+    exit 1
+  fi
 else
     file_list=($file_pattern)
 fi
 
-# Check if there are any files to process
-if [ "${#file_list[@]}" -eq 0 ]; then
-    echo "No .circom files found in subfolder '$subfolder'."
-    exit 1
-fi
-
-# Function to process a single circuit file
-process_file() {
-    local file="$1"
-    echo " "
-    echo -e "\033[1;34m==================================================\033[0m"
-    echo -e "\033[1;34mProcessing file: $file\033[0m"
-    echo -e "\033[1;34m==================================================\033[0m"
-
-    # Start timing
-    start_time=$(date +%s.%N)
-
-    circom "$file" --r1cs --wasm --sym --c
-    filename=$(basename -- "$file")
-    filename_no_extension="${filename%.*}"
-    cpp_directory="${filename_no_extension}_cpp"
-    js_directory="${filename_no_extension}_js"
-    (
-        cd "$cpp_directory" || exit
-        echo -e "\033[1;32mRunning make in $cpp_directory\033[0m"
-        make
-    )
-
-    # End timing and report
-    end_time=$(date +%s.%N)
-    elapsed_time=$(echo "$end_time - $start_time" | bc)
-    echo -e "\033[1;34mTime taken: $elapsed_time seconds\033[0m"
-}
-
 # Iterate over and process each file
 for file in "${file_list[@]}"; do
-    process_file "$file"
+  echo -e "\033[1mBuilding circuit: ${file}\033[0m"
+  circom "$file" --r1cs --wasm
 done

89b9044926ca1e1010d33af802fc01fa080dfed1
Piotr Mikolajczyk <piomiko41@gmail.com>
2025-04-02 15:11:25 +0200

    [Marketplace] Merge modules

--- marketplace/scenarios/register_devices.py
+++ marketplace/scenarios/register_devices.py
@@ -1,10 +1,10 @@
 from vimz_marketplace_sdk.account import get_actor
-from vimz_marketplace_sdk.deploy import deploy_contract
+from vimz_marketplace_sdk.chain import deploy_contract
 
 
 def main():
     device_registry_admin = get_actor("device_registry_admin")
-    
+
     deploy_contract("DeviceRegistry", device_registry_admin)
 
     camera_brand_1 = get_actor("camera_brand_1")
--- marketplace/vimz_marketplace_sdk/chain.py
+++ marketplace/vimz_marketplace_sdk/chain.py
@@ -1,10 +1,31 @@
 import os
 
 from web3 import Web3
+from web3.contract import Contract
 from web3.middleware import SignAndSendRawMiddlewareBuilder
 from web3.types import Wei
 
+from vimz_marketplace_sdk.artifacts import load_artifact
 from vimz_marketplace_sdk.types import Actor
+
+
+def deploy_contract(contract_name: str, deployer: Actor, *constructor_args, **constructor_kwargs) -> Contract:
+    print(f"Deploying contract '{contract_name}'...")
+    artifact = load_artifact(contract_name)
+
+    w3 = get_web3()
+    w3.middleware_onion.inject(SignAndSendRawMiddlewareBuilder.build(deployer.account()), layer=0)
+    ContractCls = w3.eth.contract(abi=artifact["abi"], bytecode=artifact["bytecode"]["object"])
+
+    tx_hash = ContractCls.constructor(*constructor_args, **constructor_kwargs).transact()
+    receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+
+    print(f"Contract '{contract_name}' deployed at address: {receipt.contractAddress}")
+
+    return w3.eth.contract(
+        address=receipt.contractAddress,
+        abi=artifact["abi"]
+    )
 
 
 def send_eth(from_actor: Actor, to_actor: Actor, value_wei: Wei):

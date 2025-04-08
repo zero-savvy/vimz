5c62693ed9f5485846b69b496a524fb983846730
Piotr Mikolajczyk <piomiko41@gmail.com>
2025-04-08 13:50:48 +0200

    [Marketplace] Scenarios: deployment utils

---
+++ marketplace/scenarios/__init__.py
@@ -0,0 +1,36 @@
+from vimz_marketplace_sdk.chain import get_actor, Actor
+from vimz_marketplace_sdk.contracts.creator_registry import CreatorRegistry
+from vimz_marketplace_sdk.contracts.device_registry import DeviceRegistry
+from vimz_marketplace_sdk.creator import default_creators
+from vimz_marketplace_sdk.device import Device, default_brands
+
+
+def prepare_device_registry() -> (DeviceRegistry, Device):
+    """
+    Prepare a device registry and register a default brand and device.
+    :return: A tuple containing the device registry and the registered device.
+    """
+    device_registry_admin = get_actor("device_registry_admin")
+    registry = DeviceRegistry.deploy(device_registry_admin)
+
+    brand = next(default_brands())
+    registry.register_brand(device_registry_admin, brand)
+
+    device = brand.get_new_device()
+    registry.register_device(brand, device)
+
+    return registry, device
+
+
+def prepare_creator_registry() -> (CreatorRegistry, Actor):
+    """
+    Prepare a creator registry and register a default creator.
+    :return: A tuple containing the creator registry and the registered creator.
+    """
+    creator_registry_admin = get_actor("creator_registry_admin")
+    registry = CreatorRegistry.deploy(creator_registry_admin)
+
+    creator = next(default_creators())
+    registry.register_creator(creator_registry_admin, creator)
+
+    return registry, creator
--- marketplace/scenarios/register_new_asset.py
+++ marketplace/scenarios/register_new_asset.py
@@ -1,35 +1,9 @@
 from datetime import datetime, UTC
 
-from vimz_marketplace_sdk.chain import get_actor, Actor
+from scenarios import prepare_device_registry, prepare_creator_registry
+from vimz_marketplace_sdk.chain import get_actor
 from vimz_marketplace_sdk.contracts.asset_gateway import AssetGateway
-from vimz_marketplace_sdk.contracts.creator_registry import CreatorRegistry
-from vimz_marketplace_sdk.contracts.device_registry import DeviceRegistry
-from vimz_marketplace_sdk.creator import default_creators
-from vimz_marketplace_sdk.device import Device, default_brands
 from vimz_marketplace_sdk.types import License
-
-
-def prepare_device_registry() -> (DeviceRegistry, Device):
-    device_registry_admin = get_actor("device_registry_admin")
-    registry = DeviceRegistry.deploy(device_registry_admin)
-
-    brand = next(default_brands())
-    registry.register_brand(device_registry_admin, brand)
-
-    device = brand.get_new_device()
-    registry.register_device(brand, device)
-
-    return registry, device
-
-
-def prepare_creator_registry() -> (CreatorRegistry, Actor):
-    creator_registry_admin = get_actor("creator_registry_admin")
-    registry = CreatorRegistry.deploy(creator_registry_admin)
-
-    creator = next(default_creators())
-    registry.register_creator(creator_registry_admin, creator)
-
-    return registry, creator
 
 
 def main():

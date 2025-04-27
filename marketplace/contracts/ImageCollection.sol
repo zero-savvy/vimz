ab48994b18d7a79d9ee12780fc86b59cd4c25dad
Piotr Mikolajczyk <piomiko41@gmail.com>
2025-04-28 00:16:55 +0200

    [Marketplace] Image collection

---
+++ marketplace/contracts/ImageCollection.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {ERC721} from "openzeppelin-contracts/token/ERC721/ERC721.sol";
+
+contract ImageCollection is ERC721 {
+    // ------------------------------------ TYPES ------------------------------------ //
+
+    struct CollectionData {
+        uint256[] rootHashes;
+    }
+
+    // ------------------------------------ STORAGE ------------------------------------ //
+
+    address public immutable marketplace;
+
+    mapping(uint256 => CollectionData) private collections;
+    uint256 private counter;
+
+    // ------------------------------------ MODIFIERS ------------------------------------ //
+
+    modifier onlyMarketplace() {
+        require(msg.sender == marketplace, "Only marketplace can call this");
+        _;
+    }
+
+    // ------------------------------------ PUBLIC API ------------------------------------ //
+
+    constructor(address _marketplace) ERC721("CollectionPass","CPASS") {
+        marketplace = _marketplace;
+    }
+
+    function mint(
+        address owner,
+        uint256[] calldata roots
+    ) external onlyMarketplace returns (uint256 collectionId) {
+        collectionId = ++counter;
+        _safeMint(owner, collectionId);
+        collections[collectionId] = CollectionData(roots);
+    }
+}

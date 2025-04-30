59a2ca89ea7981969afcf4b34d488bfcc456ea4e
Piotr Mikolajczyk <piomiko41@gmail.com>
2025-04-30 09:59:34 +0200

    [Marketplace] Docs: `Registries`

--- marketplace/contracts/CreatorRegistry.sol
+++ marketplace/contracts/CreatorRegistry.sol
@@ -1,39 +1,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-/**
- * @title Creator Registry
- * @dev This contract allows the registration of verified content creators.
- *      Only creators with valid KYC can be registered, ensuring that only trusted
- *      actors are permitted to upload and claim ownership of new images.
- */
+/// @dev This contract allows the registration of verified content creators.
+/// Only creators with valid KYC can be registered, ensuring that only trusted
+/// actors are permitted to register new images and their editions.
 contract CreatorRegistry {
     // ------------------------------------ TYPES ------------------------------------ //
 
-    /// @dev Struct to hold creator details.
+    /// @notice Struct to hold creator details.
+    /// @param kycExpiry Timestamp until which the creator's KYC is valid.
+    /// @param contactInfo Contact information for the creator.
+    /// @param exists Ensures creator existence in mapping
     struct Creator {
-        /// Timestamp until which the creator's KYC is valid.
         uint256 kycExpiry;
-        /// Contact information for the creator.
         string contactInfo;
-        /// Ensures creator existence in mapping
         bool exists;
     }
 
     // ------------------------------------ STORAGE ------------------------------------ //
 
     /// @notice Address of the contract administrator (the deployer)
-    address public admin;
+    address public immutable admin;
 
     /// @notice Set of registered creators identified by their Ethereum addresses.
     mapping(address => Creator) public creators;
 
     // ------------------------------------ EVENTS ------------------------------------ //
 
-    /// @notice Event emitted when a new creator is registered.
+    /// @notice Emitted when a new creator is registered.
     /// @param creator The Ethereum address of the newly registered creator.
     /// @param kycExpiry The expiration timestamp of the creator's KYC.
-    event CreatorRegistered(address indexed creator, uint256 kycExpiry);
+    event CreatorRegistered(address creator, uint256 kycExpiry);
 
     // ------------------------------------ MODIFIERS ------------------------------------ //
 
@@ -45,20 +42,15 @@ contract CreatorRegistry {
 
     // ------------------------------------ PUBLIC API ------------------------------------ //
 
-    /**
-     * @notice Constructor sets the deployer as the contract administrator.
-     */
+    /// @notice Constructor sets the deployer as the contract administrator.
     constructor() {
         admin = msg.sender;
     }
 
-    /**
-     * @notice Registers a new verified creator.
-     * @dev Only the administrator can register a new creator.
-     * @param creatorAddr The Ethereum address of the creator being registered.
-     * @param kycExpiry The timestamp indicating when the creator's KYC expires.
-     * @param contactInfo The contact information for the creator.
-     */
+    /// @notice Registers a new verified creator. Only the administrator can register a new creator.
+    /// @param creatorAddr The Ethereum address of the creator being registered.
+    /// @param kycExpiry The timestamp indicating when the creator's KYC expires.
+    /// @param contactInfo The contact information for the creator.
     function registerCreator(
         address creatorAddr,
         uint256 kycExpiry,
@@ -76,11 +68,9 @@ contract CreatorRegistry {
         emit CreatorRegistered(creatorAddr, kycExpiry);
     }
 
-    /**
-     * @notice Verifies whether a creator is registered and their KYC is still valid.
-     * @param creatorAddr The Ethereum address of the creator to verify.
-     * @return True if the creator is registered and KYC is valid, false otherwise.
-     */
+    /// @notice Verifies whether a creator is registered and their KYC is still valid.
+    /// @param creatorAddr The Ethereum address of the creator to verify.
+    /// @return `true` if the creator is registered and KYC is valid, `false` otherwise.
     function verifyCreator(address creatorAddr) external view returns (bool) {
         Creator memory creator = creators[creatorAddr];
         return creator.exists && block.timestamp < creator.kycExpiry;
--- marketplace/contracts/DeviceRegistry.sol
+++ marketplace/contracts/DeviceRegistry.sol
@@ -1,30 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-/**
- * @title Device Registry
- * @dev This contract allows trusted camera manufacturers (registrars) to register
- *      C2PA-compliant cameras on-chain. The registered devices' keys can later be used
- *      to verify the authenticity of cryptographic signatures.
- */
+/// @notice This contract allows trusted camera manufacturers (registrars) to register C2PA-compliant cameras on-chain.
+/// The registered devices' keys can later be used to verify the authenticity of cryptographic signatures.
 contract DeviceRegistry {
     // ------------------------------------ TYPES ------------------------------------ //
 
-    /// @dev Struct to hold device details
+    /// @notice Holds device details
+    /// @param registrar The brand that registered this device
     struct Device {
-        /// The brand that registered this device
         address registrar;
-        /// Ensures device existence in mapping
-        bool exists;
     }
 
     // ------------------------------------ STORAGE ------------------------------------ //
 
     /// @notice Address of the contract administrator (the deployer)
-    address public admin;
+    address public immutable admin;
 
-    /// @notice Set of approved registrars (trusted manufacturers)
-    /// @dev Only admin can add a registrar, and only registrars can register devices.
+    /// @notice Set of approved registrars (trusted manufacturers). Only admin can add a registrar, and only registrars
+    /// can register devices.
     mapping(address => bool) public registrars;
 
     /// @notice Mapping of registered devices identified by their public keys (represented by Ethereum addresses)
@@ -32,14 +26,14 @@ contract DeviceRegistry {
 
     // ------------------------------------ EVENTS ------------------------------------ //
 
-    /// @notice Event emitted when a new registrar (manufacturer) is added
+    /// @notice Emitted when a new registrar (brand) is added
     /// @param registrar The Ethereum address of the newly added registrar
     event RegistrarAdded(address registrar);
 
-    /// @notice Event emitted when a new device (camera) is registered
+    /// @notice Emitted when a new device (camera) is registered
     /// @param device The public key (represented by an Ethereum address) of the newly registered device
     /// @param registrar The Ethereum address of the manufacturer who registered the device
-    event DeviceRegistered(address device, address indexed registrar);
+    event DeviceRegistered(address device, address registrar);
 
     // ------------------------------------ MODIFIERS ------------------------------------ //
 
@@ -57,18 +51,13 @@ contract DeviceRegistry {
 
     // ------------------------------------ PUBLIC API ------------------------------------ //
 
-    /**
-     * @notice Constructor sets the deployer as the contract administrator
-     */
+    /// @notice Constructor sets the deployer as the contract administrator
     constructor() {
-        admin = msg.sender; // Assign contract deployer as admin
+        admin = msg.sender;
     }
 
-    /**
-     * @notice Adds a new trusted registrar (camera manufacturer)
-     * @dev Only the administrator can add registrars.
-     * @param registrar The Ethereum address of the manufacturer being added
-     */
+    /// @notice Adds a new trusted registrar (camera manufacturer). Only the administrator can add registrars.
+    /// @param registrar The Ethereum address of the manufacturer being added
     function registerRegistrar(address registrar) external onlyAdmin {
         require(!registrars[registrar], "Already a registrar");
 
@@ -76,31 +65,23 @@ contract DeviceRegistry {
         emit RegistrarAdded(registrar);
     }
 
-    /**
-     * @notice Registers a new verified camera device
-     * @dev Only an approved registrar can register a new device.
-     * @param devicePubKey The Ethereum address of the device being registered
-     */
+    /// @notice Registers a new verified camera device. Only an approved registrar can register a new device.
+    /// @param devicePubKey The Ethereum address of the device being registered
     function registerDevice(address devicePubKey) external onlyRegistrar {
-        require(!devices[devicePubKey].exists, "Device already registered");
-        devices[devicePubKey] = Device({registrar: msg.sender, exists: true});
+        require(devices[devicePubKey].registrar != address(0), "Device already registered");
+        devices[devicePubKey] = Device({registrar: msg.sender});
         emit DeviceRegistered(devicePubKey, msg.sender);
     }
 
-    /**
-     * @notice Verifies whether a given signature was produced by a registered device
-     * @dev Uses Ethereum's `ecrecover` to verify the signer of a message.
-     * @param messageHash The hashed message that was signed
-     * @param signature The cryptographic signature produced by the device
-     * @param deviceAddress The Ethereum address of the device that supposedly signed the message
-     * @return `true` if the signature is valid, `false` otherwise
-     */
-    function verifyDeviceSignature(bytes32 messageHash, bytes memory signature, address deviceAddress)
-        external
-        view
-        returns (bool)
+    /// @notice Verifies whether a given signature was produced by a registered device
+    /// @dev Uses Ethereum's `ecrecover` to verify the signer of a message.
+    /// @param messageHash The hashed message that was signed
+    /// @param signature The cryptographic signature produced by the device
+    /// @param deviceAddress The Ethereum address of the device that supposedly signed the message
+    /// @return `true` if the signature is valid, `false` otherwise
+    function verifyDeviceSignature(bytes32 messageHash, bytes memory signature, address deviceAddress) external view returns (bool)
     {
-        require(devices[deviceAddress].exists, "Device not found");
+        require(devices[deviceAddress].registrar != address(0), "Device not found");
         // Recover the signer address from the signature
         address signer = recoverSigner(messageHash, signature);
         // Check if the recovered signer matches the registered device address
@@ -109,13 +90,11 @@ contract DeviceRegistry {
 
     // ------------------------------------ INTERNALS ------------------------------------ //
 
-    /**
-     * @notice Recovers the Ethereum address that signed a given message
-     * @dev Uses `ecrecover`, which is a built-in Solidity function for signature recovery.
-     * @param messageHash The hashed message that was signed
-     * @param signature The cryptographic signature
-     * @return The Ethereum address of the signer
-     */
+    /// @notice Recovers the Ethereum address that signed a given message
+    /// @dev Uses `ecrecover`, which is a built-in Solidity function for signature recovery.
+    /// @param messageHash The hashed message that was signed
+    /// @param signature The cryptographic signature
+    /// @return The Ethereum address of the signer
     function recoverSigner(bytes32 messageHash, bytes memory signature) private pure returns (address) {
         require(signature.length == 65, "Invalid signature length");
 
